85fe7bf1e2a225ccc96c50cf7a6cd27b2fd37e1e
Yue2u <beloleg225@mail.ru>
2024-05-06 19:20:13 +0700

    Add main bot file

---
+++ app/main.py
@@ -0,0 +1,17 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+
+from config import settings
+
+
+async def main():
+    bot = Bot(token=settings.BOT_TOKEN, parse_mode="HTML")
+    dp = Dispatcher(storage=MemoryStorage())
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
